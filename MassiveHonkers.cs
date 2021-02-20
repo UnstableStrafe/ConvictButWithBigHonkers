e4988fb1143e77fab5172bad93d90042a8ddfc7f
UnstableStrafe <blitzgamingonline@gmail.com>
2021-02-20 16:09:29 -0500

    I hate myself so much. I spent a few hours on this fucking mistake

---
+++ MassiveHonkers.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gungeon;
+using UnityEngine;
+using ItemAPI;
+namespace ConvictButWithBigHonkers
+{
+    class HonkerPistol : AdvancedGunBehaviour
+    {
+        public static void Add()
+        {
+            Gun BRevolv = (PickupObjectDatabase.GetById(80) as Gun);
+
+            Gun gun = ETGMod.Databases.Items.NewGun("Budget Honker", "budget_honker");
+            Game.Items.Rename("outdated_gun_mods:budget_honker", "honkies:budget_honker");
+            gun.gameObject.AddComponent<HonkerPistol>();
+            gun.SetShortDescription("Affordable Arms");
+            gun.SetLongDescription("This trumpet was brought to the Gungeon by an infamous fugitive.\n\nProvided by the Hegemony Regional Magistrate. The Convict won their plea to face the Gungeon in lieu of life imprisonment; undo their crimes, or face an eternity in Gungeon. With no cost to the state, it was an acceptable arrangement.\n\nCheaply made and prone to jams, the trumpet used by only the most desperate Gungeoneers.");
+            gun.SetupSprite(null, "budget_honker_idle_001", 8);
+            gun.SetAnimationFPS(gun.shootAnimation, 10);
+            gun.SetAnimationFPS(gun.reloadAnimation, 10);
+            gun.AddProjectileModuleFrom(BRevolv, true, false);
+
+            gun.DefaultModule.ammoType = BRevolv.AmmoType;
+            gun.DefaultModule.ammoCost = BRevolv.DefaultModule.ammoCost;
+            gun.DefaultModule.shootStyle = BRevolv.DefaultModule.shootStyle;
+            gun.DefaultModule.sequenceStyle = BRevolv.DefaultModule.sequenceStyle;
+            gun.DefaultModule.angleVariance = BRevolv.DefaultModule.angleVariance;
+            gun.DefaultModule.cooldownTime = BRevolv.DefaultModule.cooldownTime;
+            gun.DefaultModule.numberOfShotsInClip = BRevolv.DefaultModule.numberOfShotsInClip;
+            gun.reloadTime = BRevolv.reloadTime;            
+            gun.gunSwitchGroup = BRevolv.gunSwitchGroup;
+            gun.muzzleFlashEffects = BRevolv.muzzleFlashEffects;
+            gun.SetBaseMaxAmmo(BRevolv.GetBaseMaxAmmo());
+            gun.quality = BRevolv.quality;
+            gun.encounterTrackable.EncounterGuid = "i hate you so fucking much you stupid pervs";
+            gun.sprite.IsPerpendicular = true;
+            gun.barrelOffset.transform.localPosition = new Vector3(1.4375f, 0.34375f, 0f);
+            gun.gunClass = BRevolv.gunClass;
+            gun.InfiniteAmmo = BRevolv.InfiniteAmmo;
+            Projectile projectile = UnityEngine.Object.Instantiate<Projectile>(gun.DefaultModule.projectiles[0]);
+            projectile.gameObject.SetActive(false);
+            FakePrefab.MarkAsFakePrefab(projectile.gameObject);
+            UnityEngine.Object.DontDestroyOnLoad(projectile);
+            gun.DefaultModule.projectiles[0] = projectile;
+            projectile.transform.parent = gun.barrelOffset;
+            projectile.baseData.damage *= 1f;
+            projectile.baseData.speed *= 1f;
+            projectile.baseData.force *= 1f;
+
+
+            BRevolv.quality = PickupObject.ItemQuality.EXCLUDED;
+            ETGMod.Databases.Items.Add(gun, null, "ANY");
+        }
+        private bool HasReloaded;
+        protected override void Update()
+        {
+            base.Update();
+            if (gun.CurrentOwner)
+            {
+
+                if (gun.PreventNormalFireAudio)
+                {
+                    this.gun.PreventNormalFireAudio = true;
+                }
+                if (!gun.IsReloading && !HasReloaded)
+                {
+                    this.HasReloaded = true;
+                }
+            }
+        }
+        public HonkerPistol()
+        {
+
+        }
+    }
+    class HonkerShotgun: AdvancedGunBehaviour
+    {
+        public static void Add()
+        {
+            Gun SHonk = (PickupObjectDatabase.GetById(202) as Gun);
+
+            Gun gun = ETGMod.Databases.Items.NewGun("Sawed-off Honker", "sawed-off_honker");
+            Game.Items.Rename("outdated_gun_mods:sawed-off_honker", "honkies:sawed-off_honker");
+            gun.gameObject.AddComponent<HonkerShotgun>();
+            gun.SetShortDescription("No Butts About It");
+            gun.SetLongDescription("A trombone modified for easy concealment. The shorter barrel widens the spread, but up-close, it's just as deadly as its full-barreled cousin.");
+            gun.SetupSprite(null, "sawed-off_honker_idle_001", 8);
+            gun.SetAnimationFPS(gun.shootAnimation, 10);
+            gun.SetAnimationFPS(gun.reloadAnimation, 10);
+            for (int i = 0; i < 4; i++)
+            {
+                GunExt.AddProjectileModuleFrom(gun, SHonk, true, false);
+            }
+            foreach (ProjectileModule projectileModule in gun.Volley.projectiles)
+            {
+                gun.DefaultModule.ammoType = SHonk.AmmoType;
+                gun.DefaultModule.ammoCost = SHonk.DefaultModule.ammoCost;
+                gun.DefaultModule.shootStyle = SHonk.DefaultModule.shootStyle;
+                gun.DefaultModule.sequenceStyle = SHonk.DefaultModule.sequenceStyle;
+                gun.DefaultModule.angleVariance = SHonk.DefaultModule.angleVariance;
+                gun.DefaultModule.cooldownTime = SHonk.DefaultModule.cooldownTime;
+                gun.DefaultModule.numberOfShotsInClip = SHonk.DefaultModule.numberOfShotsInClip;
+                Projectile projectile = UnityEngine.Object.Instantiate<Projectile>(projectileModule.projectiles[0]);
+                projectile.gameObject.SetActive(false);
+                FakePrefab.MarkAsFakePrefab(projectile.gameObject);
+                UnityEngine.Object.DontDestroyOnLoad(projectile);
+                gun.DefaultModule.projectiles[0] = projectile;
+                projectile.transform.parent = gun.barrelOffset;
+                projectile.baseData.damage *= 1f;
+                projectile.baseData.speed *= 1f;
+                projectile.baseData.force *= 1f;
+
+                bool flag = projectileModule == gun.DefaultModule;
+                if (flag)
+                {
+                    projectileModule.ammoCost = 1;
+                }
+                else
+                {
+                    projectileModule.ammoCost = 0;
+                }
+            }
+            
+            gun.reloadTime = SHonk.reloadTime;
+            gun.gunSwitchGroup = SHonk.gunSwitchGroup;
+            gun.muzzleFlashEffects = SHonk.muzzleFlashEffects;
+            gun.SetBaseMaxAmmo(SHonk.GetBaseMaxAmmo());
+            gun.quality = SHonk.quality;
+            gun.encounterTrackable.EncounterGuid = "why did i make this fucking mod i hate it whyyyyy";
+            gun.sprite.IsPerpendicular = true;
+            gun.barrelOffset.transform.localPosition = new Vector3(1.625f, 0.5f, 0f);
+            gun.gunClass = SHonk.gunClass;
+
+            SHonk.quality = PickupObject.ItemQuality.EXCLUDED;
+            
+            ETGMod.Databases.Items.Add(gun, null, "ANY");
+        }
+        private bool HasReloaded;
+        protected override void Update()
+        {
+            base.Update();
+            if (gun.CurrentOwner)
+            {
+
+                if (gun.PreventNormalFireAudio)
+                {
+                    this.gun.PreventNormalFireAudio = true;
+                }
+                if (!gun.IsReloading && !HasReloaded)
+                {
+                    this.HasReloaded = true;
+                }
+            }
+        }
+        public HonkerShotgun()
+        {
+
+        }
+    }
+}
---
+++ YouSickPervs.cs
@@ -0,0 +1,119 @@
+﻿using ItemAPI;
+using UnityEngine;
+using Gungeon;
+using System;
+using MonoMod;
+using System.Collections;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
+using System.Collections.Generic;
+namespace ConvictButWithBigHonkers
+{
+
+    public class StartThisCursedMod : ETGModule
+    {
+        public static readonly string Color = "#FCA4E2";
+
+        public override void Init()
+        {
+
+        }
+        public override void Start()
+        {
+            FakePrefabHooks.Init();
+            ItemBuilder.Init();
+            
+            HonkerPistol.Add();
+            HonkerShotgun.Add();
+            Hooks();
+            Log("Convict now has massive honkers, you sick fuck.", Color);
+           
+        }
+
+        
+        public static void Log(string text, string color = "FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+        public static void Hooks()
+        {
+            Hook Load = new Hook(typeof(PlayerController).GetMethod("DoInitialFallSpawn", BindingFlags.Instance | BindingFlags.Public), typeof(StartThisCursedMod).GetMethod("OnChamberEnter"));
+            Hook OtherLoad = new Hook(typeof(PlayerController).GetMethod("DoSpinfallSpawn", BindingFlags.Instance | BindingFlags.Public), typeof(StartThisCursedMod).GetMethod("OnChamberEnter"));
+            
+        }
+        
+        public static void OnChamberEnter(Action<PlayerController, float> orig, PlayerController self, float invisibleDelay)
+        {
+            bool? isForPlayerOne = null;
+            if (GameManager.Instance.PrimaryPlayer == self)
+            {
+                isForPlayerOne = true;
+            }
+            else if (GameManager.Instance.SecondaryPlayer == self)
+            {
+                isForPlayerOne = false;
+            }
+            orig(self, invisibleDelay);
+            if (isForPlayerOne != null)
+            {
+                GameManager.Instance.StartCoroutine(waitAndExecuteChamberEnter(isForPlayerOne.Value));
+            }
+        }
+        private static IEnumerator waitAndExecuteChamberEnter(bool isForPlayerOne)
+        {
+            PlayerController player = isForPlayerOne ? GameManager.Instance.PrimaryPlayer : GameManager.Instance.SecondaryPlayer;
+            while (player.CurrentInputState != PlayerInputState.AllInput || ETGModConsole.Instance.GUI.Visible)
+            {
+                yield return null;
+            }
+            
+            if (!hasTakenDamage() && GameManager.Instance.CurrentFloor == getStartingFloor())
+            {
+                if (player.characterIdentity == PlayableCharacters.Convict)
+                {
+                    player.inventory.DestroyAllGuns();
+
+                    player.inventory.AddGunToInventory(ETGMod.Databases.Items["sawed-off_honker"] as Gun, false);
+                    player.inventory.AddGunToInventory(ETGMod.Databases.Items["budget_honker"] as Gun, true);
+                }
+            }
+            
+        }
+        private static bool hasTakenDamage()
+        {
+            foreach (var player in GameManager.Instance.AllPlayers)
+            {
+                if (player.HasTakenDamageThisRun)
+                    return true;
+            }
+            return false;
+        }
+        private static int getStartingFloor()
+        {
+            int nextLevelIndex;
+            if (GameManager.Instance.TargetQuickRestartLevel != -1)
+            {
+                nextLevelIndex = GameManager.Instance.TargetQuickRestartLevel;
+            }
+            else
+            {
+                nextLevelIndex = 1;
+                if (GameManager.Instance.CurrentGameMode == GameManager.GameMode.SHORTCUT)
+                {
+                    nextLevelIndex += GameManager.Instance.LastShortcutFloorLoaded;
+                }
+            }
+            return nextLevelIndex;
+        }
+        IEnumerator WaitForSelectFinish()
+        {
+            yield return null;
+            yield return null;
+            yield return null;
+        }
+        public override void Exit()
+        {
+            
+        }
+    }
+}
